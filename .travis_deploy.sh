46f1f8f20f3a90f2b8c7216f030d0606361de536
ro <mail2benr@gmail.com>
2019-03-18 14:30:50 +0100

    Change travis_deploy script to deploy to oer and github pages

--- .travis_deploy.sh
+++ .travis_deploy.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 set -e #Exit with nonzero exit code if anything fails
 
-#openssl aes-256-cbc -K $encrypted_9d525ff4942a_key -iv $encrypted_9d525ff4942a_iv -in deploy-key.enc -out deploy-key -d
-#chmod 600 deploy-key
-#eval `ssh-agent -s`
-#ssh-add deploy-key
-
-#./node_modules/.bin/gh-pages -d _site/ -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
-
+#zip _site for oer upload
 cd _site
 zip -r ../oer.zip assets content images units index.html intro.html motivation.html search.html
 cd ..
 python oer_upload.py
+
+#switch urls in jekyll _config for github deploy
+sed -i '27,28s/^/#/' _config.yml
+sed -i '31,32s/#//' _config.yml
+
+make book && make site
+
+
+openssl aes-256-cbc -K $encrypted_9d525ff4942a_key -iv $encrypted_9d525ff4942a_iv -in deploy-key.enc -out deploy-key -d
+chmod 600 deploy-key
+eval `ssh-agent -s`
+ssh-add deploy-key
+
+./node_modules/.bin/gh-pages -d _site/ -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
+
